0f49ed4ba9e3bd2ee75f62cd232eef88802474eb
erlm <erlm@kea.dk>
2024-02-01 09:05:55 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        HTag hTag = new HTag(1);
+        hTag.setText("jeg er et h tag");
+
+        System.out.println(hTag);
+
+
+
+
+
+
+    }
+}
